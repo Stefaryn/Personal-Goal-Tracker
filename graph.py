3d12723ba9166f164fc5c90821b8e85361f1e0d5
NaserAlkhateri <Naser@alkhateri.com>
2020-02-27 22:43:27 -0800

    removed numpy, added text format

--- graph.py
+++ graph.py
@@ -2,55 +2,73 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as plt_dates
 from datetime import datetime, timedelta
-
 import time
-import numpy as np
+
 
 plt.style.use('seaborn')
 
 def build_graph(goalObj):
+
 	milestones = goalObj.prog_rec
 	timestamps = []
+	data_x = []
+	data_y = []
+	
+	#takes the date stamps and add them to a list in python's datetime format
 	for index in goalObj.time_rec:
 		date_splitted = index.split('-')
 		timestamps.append(datetime(int(date_splitted[0]),int(date_splitted[1]),int(date_splitted[2])))
 
-	data_x = np.array(timestamps[:1])
-	data_y = np.array(milestones[:1])
+	
+	data_x.append(timestamps[0])
+	data_y.append(milestones[0])
 	print(data_x, data_y)
 
-	fig = plt.figure(figsize=(9, 9))
+	fig = plt.figure(figsize=(8, 8))
 	ax = fig.add_subplot(111)
-	line, = ax.plot(data_x[:1], data_y[:1], '-o', picker=goalObj.finish)
+	line, = ax.plot(data_x, data_y, '-o', picker=goalObj.finish)
 
-	ax.set_ylim(0, goalObj.finish+1)
+	#initial display of graph's limits
+	ax.set_ylim(-0.2, goalObj.finish+1)
 	ax.set_xlim(timestamps[0] - timedelta(days=1), timestamps[-1] + timedelta(days=1))
-	#'get current figure' to show dates slanted
+	
+	#gcf stands for 'get current figure' to show dates slanted
 	plt.gcf().autofmt_xdate()
 
-
+	#graph's labels
 	ax.set_title(goalObj.name)
 	ax.set_ylabel('Milestones')
 	ax.set_xlabel('Dates')
+	
+	#lets the graph use the whole window
 	plt.tight_layout()
 
-	txt = ax.text(timestamps[1], milestones[3], '', va="center",fontsize=16,
-			bbox=dict(boxstyle="round",
-					ec=(1., 0.5, 0.5),
-					fc=(1., 0.8, 0.8),
-					)
+	txt = ax.text(timestamps[0], goalObj.finish-1, '', va="center",fontsize=16,
+			bbox=dict(boxstyle='round', color='lightskyblue')
 			)
 	#displays the message of the plot
+	def txt_format(txt_str):
+		#every 4th space is \n
+		formatted_txt = ""
+		space_count = 0
+		for char in txt_str:
+			if char == ' ':
+				space_count += 1
+			if space_count == 4:
+				space_count = 0
+				char = '\n'
+			formatted_txt += char
+		
+		return formatted_txt
+		
 	def on_pick(event):
 		line = event.artist
-		xdata, ydata = line.get_data()
 		ind = event.ind
-		print('DEBUG:on pick line:', np.array([xdata[ind], ydata[ind]]).T)
-    
-		x = xdata[ind]
-		y = ydata[ind]
-    
-		txt.set_text(goalObj.note_rec[ind[0]])
+
+		print("debug on pick event",ind)
+		
+		text = txt_format(str(goalObj.note_rec[ind[0]]))
+		txt.set_text(text)
 		fig.canvas.draw()
 		fig.canvas.flush_events()
     
@@ -67,8 +85,8 @@ def build_graph(goalObj):
 	plt.show(False)
 
 	for i in range(1, len(milestones) ):
-		data_x = np.append(data_x, timestamps[i])
-		data_y = np.append(data_y, milestones[i])
+		data_x.append(timestamps[i])
+		data_y.append( milestones[i])
     
 		line.set_xdata(data_x)
 		line.set_ydata(data_y)
@@ -79,5 +97,13 @@ def build_graph(goalObj):
 		print(data_x, data_y)
 		time.sleep(.25)
 
+
 	plt.show()
+	
+	
+	
+			
+	
+	
+	
 
--- test_graph.py
+++ test_graph.py
@@ -1,24 +1,23 @@
 import graph
 import goal
 
-# class Goal:
-    # def __init__(self, name: str, finish: int, progress: int = 0):
-        # self.name = name
-        # self.finish = finish
-        # self.progress = progress
-        # self.time_rec = []
-        # self.prog_rec = []
-        # self.note_rec = []
+
+
 
 
 def init_goal():
 	goalOne = goal.Goal("Horse Riding", 9)
-
+	goalOne.progress = 1
+	goalOne.time_rec = ['2020-02-10']
+	goalOne.prog_rec = [0]
+	goalOne.note_rec = ['goal created']
+	graph.build_graph(goalOne)
+	
 def prog_one():
 	goalOne = goal.Goal("Horse Riding", 9)
 	goalOne.progress = 3
 	goalOne.time_rec = ['2020-02-10','2020-02-20','2020-02-24','2020-02-27']
-	goalOne.prog_rec = [1,2,3,4]
+	goalOne.prog_rec = [0,1,2,3]
 	goalOne.note_rec = ['goal created','groomed the horse','saddled the horse','horse jumping']
 	
 	graph.build_graph(goalOne)
@@ -28,8 +27,8 @@ def prog_two():
 	goalOne = goal.Goal("Horse Riding", 9)
 	goalOne.progress = 5
 	goalOne.time_rec = ['2020-02-10','2020-02-20','2020-02-24','2020-02-27','2020-03-2','2020-03-5']
-	goalOne.prog_rec = [1,2,3,4,5,6]
-	goalOne.note_rec = ['goal created','groomed the horse','saddled the horse','horse jumping','Replaced horse shoe','Beach riding']
+	goalOne.prog_rec = [0,1,2,3,4,5]
+	goalOne.note_rec = ['goal created now lets see how the txt is formatted on screen','groomed the horse','saddled the horse','horse jumping','Replaced horse shoe','Beach riding']
 	
 	graph.build_graph(goalOne)
 	
