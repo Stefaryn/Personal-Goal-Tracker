b7dd8e333f3be249473d83d8fa349bada4f2406b
coreekymon <50118585+coreekymon@users.noreply.github.com>
2020-03-01 21:39:47 -0800

    home UI close to done

--- homeUI.py
+++ homeUI.py
@@ -9,12 +9,10 @@ import os.path
 class HomeUI:
 
     def __init__(self):
-    
-        # bool to check for second window
-        self.secondw = 0
         
         # pointer to location in list
         self.lpointer = 0
+        self.npointer = 0
         
         # initialize goal list
         self.goal_list = goal.GoalList("savefile.csv")
@@ -42,7 +40,7 @@ class HomeUI:
         
         # create list frame
         self.lframe = Frame(self.root)
-        self.lframe.grid(row=0, column=0)
+        self.lframe.grid(row=1, column=0)
         
         # create list box
         self.sbar = Scrollbar(self.lframe, orient=VERTICAL)
@@ -56,7 +54,7 @@ class HomeUI:
 
         # create button frame
         self.bframe = Frame(self.root)
-        self.bframe.grid(row=1, column=0)
+        self.bframe.grid(row=2, column=0)
         
         # create add goal button
         self.button = Button(self.bframe, text="Add Goal +", command=self.add_goal)
@@ -64,33 +62,45 @@ class HomeUI:
         
         # create other button frame
         self.eframe = Frame(self.root)
-        self.eframe.grid(row=1, column=1)
+        self.eframe.grid(row=2, column=1)
         
         # create work on goal button
-        self.ebutton = Button(self.eframe, text="Work on Goal", command=self.edit_goal)
+        self.ebutton = Button(self.eframe, text="Add Progress", command=self.add_progress)
         self.ebutton.pack()
+        
+        # create second list box
+        self.lboxf = Frame(self.root)
+        self.lboxf.grid(row=1, column=1)
+        self.scbar = Scrollbar(self.lboxf, orient=VERTICAL)
+        self.libox = Listbox(self.lboxf, yscrollcommand=self.scbar.set, height=10)
+        self.scbar.config(command=self.libox.yview)
+        self.scbar.pack(side=RIGHT, fill=Y)
+        self.libox.pack(side=LEFT, fill=BOTH, expand=1)
+        
+        self.libox.bind("<<ListboxSelect>>", self.show_note_details)
 
         # create details frame
-        self.dframe = Frame(self.root, width=200, height=200)
-        self.dframe.grid(row=0, column=1, sticky="n")
+        self.dframe = Frame(self.root)
+        self.dframe.grid(row=0, column=0, sticky="n")
         self.dframe.grid_rowconfigure(0, weight=1)
         self.dframe.grid_columnconfigure(0, weight=1)
 
         # create details
         self.text0 = StringVar()
         self.text1 = StringVar()
-        self.text2 = StringVar()
-        self.text3 = StringVar()
-        self.text4 = StringVar()
         
         self.text0.set("                                                                       ")
         self.text1.set("Welcome to Personal Goal Tracker!")
+        self.detail = Label(self.dframe, textvariable=self.text1).grid(row=0, column=0)
+        self.windowstabilizer = Label(self.dframe, textvariable=self.text0).grid(row=1, column=0)
         
-        self.windowstabilizer = Label(self.dframe, textvariable=self.text0).grid(row=0, column=0)
-        self.goalnameheader = Label(self.dframe, textvariable=self.text1).grid(row=1, column=0)
-        self.goalname = Label(self.dframe, textvariable=self.text2).grid(row=2, column=0)
-        self.goalprogheader = Label(self.dframe, textvariable=self.text3).grid(row=3, column=0)
-        self.goalprog = Label(self.dframe, textvariable=self.text4).grid(row=4, column=0)
+        # create note frame
+        self.nframe = Frame(self.root)
+        self.nframe.grid(row=1, column=2)
+        self.ntext = StringVar()
+        self.ntext.set("")
+        self.nlabel = Label(self.nframe, textvariable=self.ntext)
+        self.nlabel.pack()
         
         self.set_listbox()
         
@@ -117,56 +127,37 @@ class HomeUI:
             
     def showdetails(self, event):
         selection = self.lbox.curselection()
-        if self.secondw == 0:
-            if selection:
-                self.lpointer = int(selection[0])
-                self.text1.set("Goal:")
-                self.text2.set("%s" % (self.goal_list.goals[self.lpointer].name))
-                self.text3.set("Progress:")
-                self.text4.set("%d/%d" % (self.goal_list.goals[self.lpointer].progress, self.goal_list.goals[self.lpointer].finish))
-            
-    def edit_goal(self):
-        selection = self.lbox.curselection()
-        if self.secondw == 0:
-            if selection:
-                self.secondw = 1
-                self.lpointer = int(selection[0])
-                editwindow = Toplevel(self.root)
-                editwindow.bind("<Destroy>", self.close_second_win)
-                gname = StringVar()
-                gname.set("working on %s" % (self.goal_list.goals[self.lpointer].name))
-                swheader = Label(editwindow, textvariable=gname)
-                swheader.grid(row=0, column=0)
-                pf = StringVar()
-                pf.set("Progress:\n%d/%d" % (self.goal_list.goals[self.lpointer].progress, self.goal_list.goals[self.lpointer].finish))
-                pheader = Label(editwindow, textvariable=pf)
-                pheader.grid(row=1, column=0)
-                
-                # create list box
-                lboxf = Frame(editwindow)
-                lboxf.grid(row=2, column=0)
-                scbar = Scrollbar(lboxf, orient=VERTICAL)
-                libox = Listbox(lboxf, yscrollcommand=scbar.set, height=10)
-                scbar.config(command=libox.yview)
-                scbar.pack(side=RIGHT, fill=Y)
-                libox.pack(side=LEFT, fill=BOTH, expand=1)
-                
-                # create add progress button
-                pbutton = Button(editwindow, text="Add Progress Point", command=self.add_progress)
-                pbutton.grid(row=1, column=1)
-            else:
-                print("No goal selected")
-        else:
-            print("Second window already active")
-
-    def close_second_win(self, event):
-        self.secondw = 0
+        if selection:
+            self.lpointer = int(selection[0])
+            self.text1.set("Working on %s\nProgress:\n%d/%d" %(self.goal_list.goals[self.lpointer].name, self.goal_list.goals[self.lpointer].progress, self.goal_list.goals[self.lpointer].finish))
+            self.set_note_listbox()
+            self.ntext.set("")
         
     def add_progress(self):
-        new_prog = simpledialog.askinteger("Input", "How much progress have you made?", parent=self.root, minvalue=1, maxvalue=(self.goal_list.goals[self.lpointer].finish - self.goal_list.goals[self.lpointer].progress))
-        new_note = simpledialog.askstring("Input", "Any thoughts on this progress?", parent=self.root)
-        self.goal_list.goals[self.lpointer].update(new_prog, new_note)
-        print(self.goal_list.goals[self.lpointer])
+        selection = self.lbox.curselection()
+        if selection:
+            self.lpointer = int(selection[0])
+            new_prog = simpledialog.askinteger("Input", "How much progress have you made?", parent=self.root, minvalue=1, maxvalue=(self.goal_list.goals[self.lpointer].finish - self.goal_list.goals[self.lpointer].progress))
+            if new_prog:
+                new_note = simpledialog.askstring("Input", "Any thoughts on this progress?", parent=self.root)
+                self.goal_list.goals[self.lpointer].update(new_prog, new_note)
+                print(self.goal_list.goals[self.lpointer])
+                self.text1.set("Working on %s\nProgress:\n%d/%d" %(self.goal_list.goals[self.lpointer].name, self.goal_list.goals[self.lpointer].progress, self.goal_list.goals[self.lpointer].finish))
+                self.set_note_listbox()
+            
+    def set_note_listbox(self):
+        self.libox.delete(0,END)
+        for node in self.goal_list.goals[self.lpointer].rec:
+            self.libox.insert(END, "{node.time}".format(node=node))
+            
+    def show_note_details(self, event):
+        note_select = self.libox.curselection()
+        if note_select:
+            self.npointer = int(note_select[0])
+            self.ntext.set("%s" % (self.goal_list.goals[self.lpointer].rec[self.npointer].note))
+            
+            
+            
            
 
 
