a500bbee3d697f387d1457639300b1cb71239025
Leonie Way <lway@uoregon.edu>
2020-03-09 12:02:55 -0700

    cleaned up the comments a bit

--- goal.py
+++ goal.py
@@ -1,7 +1,7 @@
-#This is the prototype for the MVP of the goal module
+# This file contains all components of the goal module
 # written by Stephan Fields and Leonie Way
 
-#Currently written on ix-dev which uses python 3.7.3
+#written using python 3.7
 
 from datetime import datetime,date, timedelta
 import os
@@ -9,29 +9,25 @@ import csv
 
 class Node:
     '''
+    Contains information that records each time a Goal was updated
 
     '''
     def __init__(self, progress: int, time: date, note: str):
         self.progress = progress
         self.time = time
         self.note = note
-        #self.survey = survey
        
-        
+# -------------------------------------------------------------------------------
 
 class Goal:
     '''
-
+    an object that records the state of a goal.
     '''
-    # I'm tempted to basically overload this init so that it can either take this info or a file name
-    # But for now that would end up being harder to read so I will make a specific load function
     def __init__(self, name: str, finish: int, progress: int = 0):
         self.name = name
         self.finish = finish
         self.progress = progress
-        #self.survey = survey
         self.rec = []
-        #self.survey = []
 
 
     def __repr__(self) -> str:
@@ -39,8 +35,7 @@ class Goal:
 
     def update(self, change: int, note: str = ""):
         self.progress += change
-
-        #self.survey = survey
+        
         #record goal update for graphing
         self.rec.append( Node(self.progress, date.today(), note) )
         
@@ -65,7 +60,7 @@ class Goal:
 
                     
             csvfile.close()
-            #self.progress = len(self.time_rec)
+            
 
         else:
             print("DEBUG: {} file not found".format(filename))
@@ -85,21 +80,18 @@ class Goal:
 
             for p in self.rec:
                 filewriter.writerow([p.progress, p.time.toordinal(), p.note])
-        csvfile.close()
-        
+        csvfile.close()     
 
-
+# -------------------------------------------------------------------------------
 
 class GoalList:
     '''
-
+    a list of goals that can save itself
     '''
     def __init__(self, savefile: str):
         self.goals = []
         self.savefile = savefile
 
-    # TODO: These will eventually be in charge of most of the loading and saving probably, and I think Leonie wanted to use
-    # Sort of saved info file with the names of all the goals, for now I am leaving this empty
 
     def load(self):
         # read in and reinitialize all saved goals adding them to goals
